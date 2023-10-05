5baed8188bbd132bd00a1bf9ba42e7b022933018
fullan42 <hasan_-1@outlook.com>
2023-10-05 03:00:54 +0300

    Initial commit

---
+++ src/test/java/com/example/freetrader/FreeTraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freetrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeTraderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
